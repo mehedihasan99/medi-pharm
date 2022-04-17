33b6eba9a45c3c6042c93560a1dfb199783fdc1a
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-04-17 22:54:57 +0600

    design login component

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './Login.css'
 const Login = () => {
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className='container main-container'>
+            <form className='form-container'>
+                <input type="email" name="email" id="" placeholder='Enter Your Email' />
+                <input type="password" name="password" id="" placeholder='Enter Your Password' />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css'
 const Footer = () => {
     return (
-        <footer className='text-center mt-5'>
+        <footer className='text-center mt-5 footer'>
             <p>&copy; Medi Pharm</p>
         </footer>
     );
