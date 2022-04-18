d31eb20c05a13a3c8c3393969e8c567e8985d015
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-04-18 14:55:11 +0600

    add about section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './About.css'
+import myPic from '../../images/Mehedi.jpg'
 const About = () => {
     return (
-        <div>
-            <h2>This is about</h2>
+        <div className="container mt-5">
+            <h2 className='text-primary text-center mb-5'>This is Mehedi hasan</h2>
+            <div className='about-container'>
+                <div>
+                    <img src={myPic} alt="" />
+                </div>
+                <div>
+                    <h2>My goal</h2>
+                    <p>My name is Mehedi Hasan and I am learning as a full-stack Web Application Developer and Software Developer, currently living in Dhaka. And I am working hard to achieve my goal.</p>
+                </div>
+            </div>
         </div>
     );
 };
